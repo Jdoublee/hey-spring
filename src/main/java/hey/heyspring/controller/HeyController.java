d6cfb8c07297848b0eec9142f720fe1bf2bb9a84
Jdoublee <hyewon3429@gmail.com>
2021-03-11 15:27:26 +0900

    api

--- src/main/java/hey/heyspring/controller/HeyController.java
+++ src/main/java/hey/heyspring/controller/HeyController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -20,4 +21,31 @@ public class HeyController {
         model.addAttribute("name", name);
         return "hey-template";
     }
+
+    @GetMapping("hey-string")
+    @ResponseBody
+    public String heyString(@RequestParam("name") String name) {
+        return "hey " + name;
+    }
+
+    @GetMapping("hey-api")
+    @ResponseBody
+    public Hey heyApi(@RequestParam("name") String name) {
+        Hey hey = new Hey();
+        hey.setName(name);
+        return hey;
+    }
+
+    static class Hey {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
